a547dc42e1012f160c62bc9c4bd2936412241dcf
domc159 <117806778+domc159@users.noreply.github.com>
2024-08-24 14:24:28 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,8 @@ makepkg -si
 yay -S --noconfirm paru
 
 cd FunkyOS
-paru -S --needed --noconfirm - < packets.txt
-mv -r ~/.config ~/.config_bak
-cp -r config ~/.config
-
-bash ./grub.sh
+paru -S --needed --noconfirm cosmic firefox os-prober grub-customizer qt5-graphicaleffects qt5-quickcontrols qt5-quickcontrols2 qt5-svgqt5-quickcontrols-nemo sddm chromium hyprland fastfetch kitty nemo rofi swappy swaync wallust waybar wlogout nerd-fonts rofi wofi code konsole power-profiles-daemon upower acpi acpid fish 
+cp ~/FunkyOs/config/* ~/.config
 
 
 
