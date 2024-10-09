faf58279007ddf19b3bc3b107f5dcab221d6b42e
domc159 <117806778+domc159@users.noreply.github.com>
2024-10-09 08:24:39 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ makepkg -si
 yay -S --noconfirm paru
 
 cd FunkyOS
-yay -S --needed --noconfirm 
+yay -S --noconfirm hyprpaper hyprpicker hypridle hyprlock hyprcursor xdg-desktop-portal-hyprland xorg-xwayland xwaylandvideobridge os-prober grub-customizer qt5-graphicaleffects qt5-quickcontrols qt5-quickcontrols2 qt5-svg qt5-quickcontrols-nemo sddm fastfetch kitty nemo rofi swappy swaync wallust waybar wlogout nerd-fonts wofi code konsole power-profiles-daemon upower acpi acpid fish uwsm
 cp -r ~/FunkyOs/config/* ~/.config/
 
 sudo chmod 777 ~/.config/hypr/UserScripts/*
