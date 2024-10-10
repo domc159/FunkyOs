018fd7ec219fdccdd72d9dee659c4c0b5c6711fd
domc159 <117806778+domc159@users.noreply.github.com>
2024-10-10 16:23:35 +0000

    paru_install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,6 @@ cd yay
 makepkg -si
 yay -S --noconfirm paru
 
-cd FunkyOS
-yay -S --noconfirm hyprpaper hyprpicker hypridle hyprlock hyprcursor xdg-desktop-portal-hyprland xorg-xwayland xwaylandvideobridge os-prober grub-customizer qt5-graphicaleffects qt5-quickcontrols qt5-quickcontrols2 qt5-svg qt5-quickcontrols-nemo sddm fastfetch kitty nemo rofi swappy swaync wallust waybar wlogout nerd-fonts wofi code konsole power-profiles-daemon upower acpi acpid fish uwsm
-cp -r ~/FunkyOs/config/* ~/.config/
-
-sudo chmod 777 ~/.config/hypr/UserScripts/*
-sudo chmod 777 ~/.config/hypr/scripts/*
 
 
 
